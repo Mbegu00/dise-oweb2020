a1777fd994e9c08cf556cae1a327f481726fc5ce
Cubilla <cubilla339@gmail.com>
2020-12-17 21:49:35 -0300

    Proyecto final no entregado

---
+++ Proyecto_final/js/logica.js
@@ -0,0 +1,29 @@
+
+
+var compra = document.querySelector('span#cantidad');
+const MILLISECONDS_OF_A_SECOND = 1000;
+
+function prueba(){
+	var ga = 100000;
+
+	compra.textContent = ga;
+
+}
+
+function myFunction() {
+  alert('Diste un clic!');
+  }
+
+
+
+prueba();
+myFunction();
+setInterval(prueba, myFunction, 3000);
+
+
+
+
+
+//var btn = document.getElementById("btn"),
+//var seco = 1000,
+//document.getElementById('cantidad').innerHTML = 'GATO';
---
+++ Tarea 6/funtion.js
@@ -0,0 +1,7 @@
+function sumar(){
+	var selec_1 = parseInt(document.getElementById.("num_1").value)
+	var selec_2 = parseInt(document.getElementById.("num_2").value)
+
+	alert(selec_1 + selec_2)
+
+}
